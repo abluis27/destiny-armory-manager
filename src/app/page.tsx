8a4a0778059dd4e6745631545109208f84327357
Luis Anzules <abluis@Luis-PC>
2025-05-04 12:15:03 +0200

    Finished the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 export default async function Home() {
   return (
     // For testing porpuses
-    <div className="w-full h-full flex justify-center items-center">
-      <p>I am the main page</p>
+    <div>
+      <div className="flex justify-center items-center
+      bg-medium-dark py-3">
+        <p className="text-sm">
+          Your companion for Destiny 2 Weapons and Perks tracking</p>
+      </div>
+      <div>
+        <p className="py-4 px-3 text-xl">Weapon List</p>
+      </div>
+      <div className="w-full h-screen flex justify-center items-center">
+        <p>I am the main page</p>
+      </div>
     </div>
   );
 }

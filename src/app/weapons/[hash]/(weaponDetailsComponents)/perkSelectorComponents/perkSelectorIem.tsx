b7209a217d8ab764bdb94b3d3463f74be9d12e47
Luis Anzules <abluis@Luis-PC>
2025-05-07 12:46:20 +0200

    Added group hover for the perk description

--- src/app/weapons/[hash]/(weaponDetailsComponents)/perkSelectorComponents/perkSelectorIem.tsx
+++ src/app/weapons/[hash]/(weaponDetailsComponents)/perkSelectorComponents/perkSelectorIem.tsx
@@ -1,28 +1,52 @@
 import { PerkSelectorItemProps } from "@/app/interfaces/weaponDetails/perkSelector/PerkSelectorItemProps";
 import { bungieBaseUrl } from "@/lib/utils"
+import { useState } from "react";
 
 const PerkSelectorItem = ({
   perk,
   isSelected,
   onSelect,
 }: PerkSelectorItemProps) => {
+    const [hovered, setHovered] = useState(false)
     const perkIcon = perk.displayProperties.icon;
     const itemType = perk.itemTypeDisplayName;
 
+    console.log(perk.displayProperties.description)
+
     if (!perkIcon || itemType.includes("Enhanced")) return null;
     return (
-        <div
-            onClick={() => onSelect(perk)}
-            className={`rounded-full p-1 cursor-pointer border-1 border-off-white transition
-            ${isSelected ? "bg-blue-500" : "bg-medium"}
-            `}
-        >
-            <img
-            src={`${bungieBaseUrl}${perkIcon}`}
-            alt={perk.displayProperties.name}
-            className="w-10 h-10 rounded-full object-cover"
-            title={perk.displayProperties.description}
-            />
+        <div className="relative inline-block">
+            <div
+                onClick={() => onSelect(perk)}
+                className={`relative rounded-full p-1 cursor-pointer border-1 border-off-white transition
+                ${isSelected ? "bg-blue-500" : "bg-medium"}
+                `}
+            >
+                <img
+                src={`${bungieBaseUrl}${perkIcon}`}
+                alt={perk.displayProperties.name}
+                className="w-10 h-10 rounded-full object-cover"
+                />
+
+                {/* Center Overlay (hover trigger) */}
+                <div className="absolute top-1/4 left-1/4 w-1/2 h-1/2 z-10" 
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                />
+            </div>
+            {/* Tooltip below the image */}
+            {hovered && (
+                <div className="absolute z-20 flex flex-col rounded-md
+                bg-medium-dark border-1 border-dark">
+                    <div className="w-full text-sm bg-dark p-3 rounded-t-md">
+                        <p>{perk.displayProperties.name}</p>
+                    </div>
+                    <div className="text-sm shadow-lg w-max max-w-xs
+                    p-3 whitespace-pre-line">
+                        <p>{perk.displayProperties.description}</p>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }

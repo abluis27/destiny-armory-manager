93fe598d536f2a7cdc479959d73b4022a401dccd
Luis Anzules <abluis@Luis-PC>
2025-05-20 12:44:42 +0200

    Change the next js condig for the bungie base url

--- src/lib/services/localStorage/useStorageState.ts
+++ src/lib/services/localStorage/useStorageState.ts
@@ -1,17 +1,30 @@
 import { useState, useEffect } from "react";
 import LocalStorageService from "./localStorage";
 
-function useStorageState<T>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [state, setState] = useState<T>(() => {
-    const saved = LocalStorageService.get<T>(key);
-    return saved !== null ? saved : initialValue;
-  });
+function useStorageState<T>(
+  key: string,
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = useState<T>(initialValue)
 
+  // Load from localStorage after the component mounts
   useEffect(() => {
-    LocalStorageService.set(key, state);
-  }, [key, state]);
+    if (typeof window !== "undefined") {
+      const saved = LocalStorageService.get<T>(key)
+      if (saved !== null) {
+        setState(saved);
+      }
+    }
+  }, [key])
 
-  return [state, setState];
+  // Save to localStorage on state change
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      LocalStorageService.set(key, state)
+    }
+  }, [key, state])
+
+  return [state, setState]
 }
 
-export default useStorageState;
+export default useStorageState
